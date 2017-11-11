458d78c410e9bde6553ffcd157b769f69398c1a0
Unknown <devpindroid@gmail.com>
2017-11-10 18:56:03 -0700

    Implemented guess letter and win/loss screens

--- Test2/DevinPiner_Test2.asm
+++ Test2/DevinPiner_Test2.asm
@@ -8,9 +8,14 @@ INCLUDE Irvine32.inc
 .data
 	;{your variables are to be defined here}
 	
+	; Prompts
+	WIN BYTE "You got the word!", 0
+	LOSE BYTE "Sorry. You have no more guesses.", 0Ah, 0Dh, "The word was ", 0
+
 	; Game variables
-	LETTER_GUESS_MAX = 11
-	WORD_GUESS_MAX = 3
+	LETTER_GUESS_MAX = 11						; the number of letter guesses to give the user
+	WORD_GUESS_MAX = 3							; the number of word guesses to give the user
+	MAX_STRING_LENGTH = 15						; the length of the longest string + 1
 
 	; Possible game strings
 	string0 BYTE "kiwi", 0
@@ -37,14 +42,17 @@ INCLUDE Irvine32.inc
 				 string7, string8, string9, string10, string11, string12, string13,
 				 string14, string15, string16, string17, string18, string19
 	strings_length BYTE 4, 5, 8, 5, 6, 6, 7, 7, 6, 6, 7, 5, 7, 12, 9, 4, 5, 6, 14, 5
+	guessed_letters BYTE LETTER_GUESS_MAX dup(0)
+	correct_guesses BYTE 0
 	TOTAL_STRINGS = 20
 
 	; Game data
-	current_string DWORD 0
-	current_string_length BYTE 0
-	current_string_guesses BYTE 14 DUP('_'), 0
-	letter_guesses BYTE LETTER_GUESS_MAX
-	word_guesses BYTE WORD_GUESS_MAX
+	current_string DWORD 0							; The word the user has to guess
+	current_string_length BYTE 0					; the words length
+	current_string_guesses BYTE 14 DUP('_'), 0		; string holding the user's correctly guessed letters
+	current_guess BYTE 15 DUP(0)					; string to hold the word guessed by the user
+	letter_guesses BYTE LETTER_GUESS_MAX			; the total number of letter guesses remaining
+	word_guesses BYTE WORD_GUESS_MAX				; the total number of word guesses remaining
 
 	; Meta game data
 	total_games BYTE 0
@@ -72,20 +80,39 @@ main PROC
 		call Clrscr
 
 	start_game:
+
+		; check if the user has guessed all the letters correctly
+		MOV AL, correct_guesses
+		CMP AL, current_string_length
+		JE WINNER
+
+		; check how many letter and word guesses are left
+		MOV AL, letter_guesses
+		CMP AL, 0
+		JA StillPlaying
+		MOV AL, word_guesses
+		CMP AL, 0
+		JE LOSER
+
+		StillPlaying:
 		MOV EDX, OFFSET current_string_guesses
 		MOVZX ECX, current_string_length
+		MOV CH, correct_guesses
+		MOV EBX, OFFSET guessed_letters
+		MOV AL, LETTER_GUESS_MAX
 		
 		call Clrscr
 
 		call PrintGuesses
 		call Crlf
 		call Crlf
-		MOV EDX, current_string
-		call WriteString
-		MOV EAX, ECX
-		call Crlf
-		call WriteDec
-		call Crlf
+		;MOV EDX, current_string
+		;call WriteString
+		;MOV EAX, ECX
+		;call Crlf
+		;MOVZX EAX, current_string_length
+		;call WriteDec
+		;call Crlf
 
 		MOV AL, letter_guesses
 		MOV AH, word_guesses
@@ -123,32 +150,89 @@ main PROC
 		Option1:
 			; Guess a letter
 			call Clrscr
+
+			CMP letter_guesses, 0
+			JE start_game
+
 			MOV EDX, OFFSET current_string_guesses
 			MOVZX ECX, current_string_length
+			MOV CH, correct_guesses
+			MOV EBX, OFFSET guessed_letters
+			MOV AL, LETTER_GUESS_MAX
 			call PrintGuesses
 			call Crlf
 			call Crlf
+
+			MOV EAX, 0
+			call GetUserChar
+			
+			; Receives: offset of array in ESI, char in AL, array length in AH
+			; Returns: 1 in AH if letter is in the array, 0 if not 
+			MOV ESI, OFFSET guessed_letters
+			MOV AH, LETTER_GUESS_MAX
+			call CheckIfLetterUsed
+
+			MOVZX ESI, letter_guesses
+			DEC ESI
+			MOV [guessed_letters+ESI], AL
+
+			CMP AH, 0
+			JE NotGuessed
+			jmp skip_checks
+
+			NotGuessed:
+			; Receives: String length in CL, guessed char in CH, game string in EDX, offset display string in EBX
+			; Returns: number of letter matches in CL
 			MOV EDX, current_string
 			MOV EBX, OFFSET current_string_guesses
-			MOVZX ECX, current_string_length
+			MOV CL, current_string_length
+			MOV CH, AL
 			call GuessALetter
+			ADD correct_guesses, CL
+
+			skip_checks:
+			DEC letter_guesses
 			jmp start_game
+
 		Option2:
 			; Guess a word
+			MOV EDX, OFFSET current_guess
+			MOVZX ECX, current_string_length
+			INC ECX
+			call ReadString
+			DEC word_guesses
 			jmp start_game
+
 		Option3:
 			; Encrypt
 			jmp start_game
-
-	call WaitMsg
+	
+	WINNER:
+		call Clrscr
+		MOV EDX, current_string
+		call WriteString
+		call Crlf
+		MOV EDX, OFFSET WIN
+		call WriteString
+		call Crlf
+		JMP Ending
+	LOSER:
+		call Clrscr
+		MOV EDX, OFFSET LOSE
+		call WriteString
+		MOV EDX, current_string
+		call WriteString
+		call Crlf
+		JMP Ending
 
 	Ending:
+	call WaitMsg
 exit
 main ENDP	; end main procedure
 
 Instructions PROC
 .data
-	words BYTE "You have 11 attempts to guess what letters are in the word", 0Ah, 0Dh,
+	words BYTE "Hangman!", 0Ah, 0Dh, "You have 11 attempts to guess what letters are in the word", 0Ah, 0Dh,
 				"and 3 attempts the guess the word. ", 0
 .code
 	MOV EDX, OFFSET words
@@ -174,10 +258,20 @@ PickGameString ENDP
 
 PrintGuesses PROC
 ; Prints out the guessed letters for the current string
-; Receives: offset of guesses string in EDX, length of the string in ECX
+; Receives: offset of guesses string in EDX, length of the string in CL, number of correct letters in CH, 
+;			offset of guessed letters in EBX, number of guessed letters in AL
 
 .data
+	left BYTE "(", 0
+	slash BYTE "/", 0
+	right BYTE ")", 0
+	left1 BYTE "[ ", 0
+	right1 BYTE "]", 0
 .code
+	PUSH EAX
+	PUSH EBX
+	PUSH ECX
+	MOVZX ECX, CL
 	MOV EBX, 0
 	MOV AL, 20h
 	call WriteChar
@@ -190,6 +284,55 @@ PrintGuesses PROC
 		call WriteChar
 		INC EBX
 		LOOP L1
+
+	POP ECX
+
+	MOV EBX, 0
+	MOV AL, 20h
+	call WriteChar
+	call WriteChar
+
+	MOV EDX, OFFSET left
+	call WriteString
+	MOVZX EAX, CH
+	call WriteDec
+	MOV EDX, OFFSET slash
+	call WriteString
+	MOVZX EAX, CL
+	call WriteDec
+	MOV EDX, OFFSET right
+	call WriteString
+
+	call Crlf
+
+	MOV AL, 20h
+	call WriteChar
+	call WriteChar
+	call WriteChar
+	MOV EDX, OFFSET left1
+	call WriteString
+
+	POP EBX
+	POP EAX
+	MOV EDX, EBX
+	MOVZX ECX, AL
+	MOV EBX, 0
+
+	L2:
+		MOV AL, [EDX+EBX]
+		CMP AL, 0
+		JE continue
+
+		call WriteChar
+		MOV AL, 20h
+		call WriteChar
+
+		continue:
+		INC EBX
+	LOOP L2
+	MOV EDX, OFFSET right1
+	call WriteString
+
 	ret
 PrintGuesses ENDP
 
@@ -220,15 +363,18 @@ PrintMainMenu PROC
 PrintMainMenu ENDP
 
 GuessALetter PROC
+; Returns the number of letter matches found in the string, and puts the guessed char in the display string
+; Receives: String length in CL, guessed char in CH, game string in EDX, offset display string in EBX
+; Returns: number of letter matches in CL
+; Requires: 
 .data
-	prompt BYTE "Guess a letter", 0Ah, 0Dh, "->", 0
 	char BYTE 0
+	correct BYTE 0
 .code
-	MOV EDX, OFFSET prompt
-	call WriteString
-	call ReadChar
-
-	MOV char, AL
+	; save the char entered by the user
+	MOV char, CH
+	MOV CH, 0
+	MOVZX ECX, CL
 
 	MOV ESI, 0
 
@@ -238,12 +384,147 @@ GuessALetter PROC
 		JNE Continue
 
 		MOV [EBX+ESI], AL
+		INC correct
 
 		Continue:
 		INC ESI
 	LOOP L1
 
+	MOV CL, correct
+	MOV correct, 0
 	ret
 GuessALetter ENDP
+
+CharToLower PROC
+; Receives: char in AL
+; Returns: char converted to lowercase
+; Requires: 
+
+	
+	; To convert a string to lowercase, loop through the string and check what range
+	; the character lands in. More the current character into AL and call IsAlpha.
+	; IsAlpha will return 1 in AH if the character is lower case, 1 in AL if it is
+	; uppercase, or 0 in both if it is not a letter. If the character is a alpha letter
+	; check if it is an uppercase letter. If so, add 32 to the uppercase letter to 
+	; convert it to lowercase. Move that new letter into the current spot to "remove"
+	; the uppercase letter. For each non-letter character, skip to the next letter
+
+	PUSH EAX							; Save the letter because IsAlpha will erase it
+	call IsAlpha						; check if the character is a letter
+
+	CMP AH, 1							; If AH is 1, it is a uppercase letter
+	POP EAX								; POP the letter out so that if it is uppercase we can use it
+	JNE continue
+
+	ADD EAX, 32d
+
+	continue:
+	ret
+CharToLower ENDP
+
+IsAlpha PROC
+; Returns true if a character is a letter A-Z or a-z
+; Recieves: The character in AL
+; Returns: 1 in AL if lowercase, 1 in AH if uppercase, 0 in both in non-alpha character
+; Requires: 
+
+	; To check if a character is a letter, we must check that it falls into the ranges
+	; 041h-05Ah (A-Z) or 061h-07Ah (a-z). To do this, if the value is less than 'A', 
+	; or greater than 'z', we know that it is not a letter. If we don't fall out of the
+	; proc after checking those, we check if the character is less than 'Z'. If it is
+	; the letter is uppercase. If not, we check that it is greater than 'a'. If it is
+	; the letter is lower case. If neither of those conditions hold, we know that it is
+	; is a special character between 'Z' and 'a'
+
+	; B Check if the char is less than A (041h)
+	CMP AL, 'A'
+	JB IsAlpha_Return_0								; Jump if al < 'A'
+	; A Check if the char is more than z (07Ah)
+	CMP AL, 'z'
+	JA IsAlpha_Return_0								; Jump if al > 'z'
+
+	; C Check if the char is less than Z (05Ah)
+	CMP AL, 'Z'
+	JBE IsAlpha_Return_Upper						; Jump if al <= 'Z'
+	; D Check if the char is more than a (061h)
+	CMP AL, 'a'
+	JAE IsAlpha_Return_Lower						; Jump if AL >= 'a'
+
+	JMP IsAlpha_Return_0
+
+	IsAlpha_Return_Lower:							; If we get here, the letter is lowercase
+		MOV EAX, 0									; Clear EAX
+		MOV AL, 1									; Move 1 into AL to flag lowercase
+		JMP IsAlpha_Exit
+	IsAlpha_Return_Upper:							; If we get here, the letter is uppercase
+		MOV EAX, 0									; Clear EAX
+		MOV AH, 1									; Move 1 into AH to flag uppercase
+		JMP IsAlpha_Exit
+	IsAlpha_Return_0:								; If we get here, the char is a special character
+		MOV EAX, 0									; Move 0 into EAX to flag a special char
+
+	IsAlpha_Exit:
+	ret
+
+IsAlpha ENDP
+
+GetUserChar PROC
+; Gets a char from the user
+; Recieves:
+; Returns: user-entered char in AL, converted to lowercase
+.data
+	p_guess_letter BYTE "Guess a letter", 0Ah, 0Dh, "->", 0
+
+.code
+	; Get the user char
+	MOV EDX, OFFSET p_guess_letter
+	call WriteString
+	call ReadChar
+
+	; Print the char to the screen so that the user can see that they entered it correctly
+	call WriteChar
+	PUSH EAX
+	MOV EAX, 100
+	call Delay
+	POP EAX
+	call CharToLower
+
+	ret
+GetUserChar ENDP
+
+CheckIfLetterUsed PROC
+; Check if a letter is in the array (it has already been guessed)
+; Receives: offset of array in ESI, char in AL, array length in AH
+; Returns: 1 in AH if letter is in the array, 0 if not
+; Requires: 
+
+.data
+	flg BYTE 0
+.code
+	MOV flg, 0
+	MOVZX ECX, AH
+	MOV EBX, 0			; iterator
+
+	L1:
+		MOV AH, [ESI+EBX]
+		CMP AH, AL
+		JNE ContinueL1
+
+		MOV flg, 1
+		JMP BreakL1
+
+		ContinueL1:
+		INC EBX
+	LOOP L1
+
+	BreakL1:
+	MOV AH, flg
+	ret
+CheckIfLetterUsed ENDP
+
+GuessAString PROC
+
+	ret
+GuessAString ENDP
 
 END main	; end of source code
